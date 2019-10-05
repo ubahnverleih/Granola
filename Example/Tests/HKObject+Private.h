e02022fa3314af38f4cb537a07a3e5bea47486f2
Mark DeLaVergne <mark.delavergne@lifeomic.com>
2019-10-05 19:43:11 -0400

    Changes to get building with Xcode 10.3

--- Example/Tests/HKObject+Private.h
+++ Example/Tests/HKObject+Private.h
@@ -21,6 +21,6 @@
 
 // source:
 // https://github.com/nst/iOS-Runtime-Headers/blob/e578efc846bd46a2d24a4fdd033cdc582323ccec/Frameworks/HealthKit.framework/HKObject.h#L55
-- (BOOL)validateForSaving:(id*)arg1;
+- (BOOL)_validateForSavingWithClientEntitlements:(id)arg1 error:(id*)arg2;
 
 @end
